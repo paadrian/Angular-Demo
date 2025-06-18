8ba765c1b390191812b316e3d77aa2478d766672
Adrian Panaite <adrianpanaite95@gmail.com>
2025-06-18 15:28:28 +0300

    add standalone app

---
+++ fourth-app/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  imports: [FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'fourth-app';
+  username: string = 'John Doe';
+}
